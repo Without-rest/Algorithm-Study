d84c417285123569037722fbd377e0f2b39b5aa2
suyeon <58283314+suyeonChoi7807@users.noreply.github.com>
2022-11-20 23:19:17 +0900

    Update ex01.java

--- 풀이/수연/1주차/ex01.java
+++ 풀이/수연/1주차/ex01.java
@@ -1,35 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class Ex01 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            StringBuilder sb = new StringBuilder();
 
-        int arr[] = new int[n];
-        for(int i=0;i<n;i++){
-            arr[i] = sc.nextInt();
-        }
-
-        int start = 0;
-        int end = 0;
-        int sum = 0;
-        for(int i=0;i<m;i++){
-            start = sc.nextInt();
-            end = sc.nextInt();
-            int cnt = end-start;
-
-            if(cnt==0){
-                sum = arr[end-1];
-            }else if(cnt==1){
-                sum = arr[start-1] + arr[end-1];
-            }else if(cnt%2==0){
-                sum = arr[(end+start)/2-1]*(cnt+1);
-            }else{
-                sum = (arr[start-1]+arr[end-1])*(cnt-1);
+            // sum arr
+            int arr[] = new int[n];
+            st = new StringTokenizer(br.readLine());
+            for(int i=0;i<n;i++){
+                arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            System.out.println(sum);
-        }
+            int sum[] = new int[n];
+            for(int i=0;i<n;i++){
+                if(i==0) {
+                    sum[i] = arr[i];
+                }else {
+                    sum[i] = arr[i] + sum[i - 1];
+                }
+            }
+
+            int startIndex = 0;
+            int endIndex = 0;
+            int res = 0;
+
+            for(int i=0;i<m;i++) {
+                st = new StringTokenizer(br.readLine());
+                startIndex = Integer.parseInt(st.nextToken()) - 1;
+                endIndex = Integer.parseInt(st.nextToken()) - 1;
+
+                if (startIndex == 0) {
+                    res = sum[endIndex];
+                } else {
+                    res = sum[endIndex] - sum[startIndex - 1];
+                }
+                sb.append(res + "\n");
+            }
+            System.out.println(sb.toString());
     }
 }
