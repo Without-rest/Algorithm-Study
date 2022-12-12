eb1a4c1fe46f9e46c864c717b660e3355e773c2c
ino5 <chero77@naver.com>
2022-12-12 23:11:05 +0900

    Update and rename Ex013.java to P013.java

--- 풀이/인호/4주차/P013.java
+++ 풀이/인호/4주차/P013.java
@@ -3,7 +3,7 @@ package baekjoon;
 import java.util.*;
 import java.io.*;
 
-public class Ex013 {
+public class P013 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
