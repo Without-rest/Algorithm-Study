ceafebca4d043c59d99c6778f98b0de87da6ea0b
suyeon <58283314+suyeonChoi7807@users.noreply.github.com>
2022-12-14 08:32:36 +0900

    Create ex14.java

---
+++ 풀이/수연/4주차/ex14.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
+            int abs1 = Math.abs(o1);
+            int abs2 = Math.abs(o2);
+
+            if(abs1 == abs2) return o1 > o2 ? 1 : -1;
+            return abs1 - abs2;
+        });
+
+        for(int i = 0 ; i < n; i++){
+            int val = Integer.parseInt(br.readLine());
+            if(val == 0){
+                if(queue.isEmpty()) System.out.println("0");
+                else System.out.println(queue.poll());
+            }else{
+                queue.add(val);
+            }
+        }
+    }
+}
