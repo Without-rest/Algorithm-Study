67520352b99a9f7c84e963451209b88a6d2dc9d6
ino5 <chero77@naver.com>
2023-01-29 22:40:23 +0900

    9주차 효율적인 해킹 실버1 풀이 (DFS 시간초과 해결 간선 탐색 방향 반대로 해서)

--- 풀이/인호/9주차/P047.java
+++ 풀이/인호/9주차/P047.java
@@ -1,13 +1,27 @@
-import java.io.*;
-import java.util.*;
+package baekjoon;
 
-// DFS. 시간초과 계속 나는데 해결 못함.. ㅜㅜ
-public class P047 {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/1325
+ * 효율적인 해킹
+ * 실버1
+ *
+ * 재귀 -> 스택으로 바꿔도 시간초과나는데 여기서 한번 간선 위치 바꿔보기. 그래서 만약 된다면 스택이냐 재귀냐는 크게 안중요하다는 결론이 나옴
+ * 
+ * 결과 => 통과함
+ */
+public class P1325_5_4_230129 {
     static int N, M;
     static ArrayList<Integer>[] E;
     static boolean[] V;
-    static int max_ansVal = Integer.MIN_VALUE;
-    static int[] answers;
+    static int[] answers = new int [10001];
+    static int answer = 0;
+    static int cnt = 0;
+    static Stack<Integer> stack = new Stack<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -15,33 +29,35 @@ public class P047 {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        V = new boolean[N+1];
         E = new ArrayList[N+1];
         for (int i = 1; i <= N; i++) {
             E[i] = new ArrayList<>();
         }
-        answers = new int[N+1];
+        // answers = new int[N+1];
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            E[b].add(a); // b->a (a는 b를 신뢰한다.)
+            E[a].add(b);
         }
 
         for (int i = 1; i <= N; i++) {
-            int s_node = i; // 시작 노드
-            V[s_node] = true;
+            // 방문 초기화
+            V = new boolean[N+1];
+            V[i] = true;
+            stack.push(i);
+            dfs();
+//            dfs(i);
+//            answers[i] = answer;
+//            answer = 0;
+        }
 
-            dfs(s_node, s_node);
-
-            // 정답 넣기
+        int max_ansVal = 0;
+        for (int i = 1; i <= N; i++) {
             if (answers[i] > max_ansVal) {
                 max_ansVal = answers[i];
             }
-            // 방문 초기화
-            V = new boolean[N+1];
         }
-
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= N; i++) {
             if (answers[i] == max_ansVal) {
@@ -52,12 +68,23 @@ public class P047 {
         bw.flush();
     }
 
-    static void dfs(int node, int s_node) {
-        for (int n_node : E[node]) {
-            if (!V[n_node]) {
-                V[n_node] = true;
-                answers[s_node]++;
-                dfs(n_node, s_node);
+    static void dfs() {
+//    static void dfs(int node) { // 재귀방식
+//        for (int n_node : E[node]) {
+//            if (!V[n_node]) {
+//                V[n_node] = true;
+//                answer++;
+//                dfs(n_node);
+//            }
+//        }
+        while(!stack.isEmpty()) {
+            int node = stack.pop();
+            for (int n_node : E[node]) {
+                if (!V[n_node]) {
+                    V[n_node] = true;
+                    stack.push(n_node);
+                    answers[n_node]++;
+                }
             }
         }
     }
